50f306a1acc22f0999af58cf2529c8d312a23aa5
Tristan Straub <tristanstraub@gmail.com>
2014-05-13 22:32:50 +1000

    Check for gcd properly

--- test/io/allthethings/blog/convert_midje_to_quickcheck_test.clj
+++ test/io/allthethings/blog/convert_midje_to_quickcheck_test.clj
@@ -8,7 +8,14 @@
 (fact "We can find the greatest common divisor of two positive integers"
   (gcd 24 18) => 6)
 
+(defn gcd? [a b gcd]
+  (= gcd
+     (apply max
+            (filter
+             (fn [x] (and (= 0 (mod a x))
+                          (= 0 (mod b x))))
+             (range 1 (+ 1 (min a b)))))))
+
 (fact-quickcheck "We can find the greatest common divisor of two positive integers"
   [a (gen/such-that #(< 0 %1) gen/pos-int) b (gen/such-that #(< 0 %1) gen/pos-int)]
-  (gcd a b) => #(and (= 0 (mod a %1))
-                     (= 0 (mod b %1))))
+  (gcd a b) => #(gcd? a b %1))
